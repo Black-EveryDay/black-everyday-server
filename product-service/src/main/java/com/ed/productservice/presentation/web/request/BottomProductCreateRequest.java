577ba3c9698c336e6499b56c4a156303df43d4fc
sdongpil <pildong94@naver.com>
2025-01-04 00:56:02 +0900

    Feat: Set up presentation layer with product registration API
    - 컨트롤러, 요청값, useCase 작성

--- product-service/src/main/java/com/ed/productservice/presentation/web/request/BottomProductCreateRequest.java
+++ product-service/src/main/java/com/ed/productservice/presentation/web/request/BottomProductCreateRequest.java
@@ -3,23 +3,48 @@ package com.ed.productservice.presentation.web.request;
 import com.ed.productservice.domain.BottomProduct;
 import com.ed.productservice.domain.ProductForCreate;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public record BottomProductCreateRequest(
         ProductCommonInfo productInfo,
-        String bottomSize,
-        int waistWidth,
-        int hipWidth,
-        int thighWidth,
-        int bottomTotalLength
+        List<BottomSizeRequest> bottomSizeRequestList
+
 ) {
+    public static class BottomSizeRequest{
+        private String bottomSize;
+        private BigDecimal waistWidth;
+        private BigDecimal hipWidth;
+        private BigDecimal thighWidth;
+        private BigDecimal bottomTotalLength;
+
+        public BottomSizeRequest(String bottomSize, BigDecimal waistWidth, BigDecimal hipWidth, BigDecimal thighWidth, BigDecimal bottomTotalLength) {
+            this.bottomSize = bottomSize;
+            this.waistWidth = waistWidth;
+            this.hipWidth = hipWidth;
+            this.thighWidth = thighWidth;
+            this.bottomTotalLength = bottomTotalLength;
+        }
+    }
+
     public BottomProduct toDomain() {
         return new BottomProduct(
                 createProductInfo(),
-                bottomSize,
-                waistWidth,
-                hipWidth,
-                thighWidth,
-                bottomTotalLength
+                createBottomSizeList()
+
         );
+    }
+
+    private List<BottomProduct.BottomSize> createBottomSizeList() {
+        return bottomSizeRequestList.stream()
+                .map(request -> new BottomProduct.BottomSize(
+                        request.bottomSize,
+                        request.waistWidth,
+                        request.hipWidth,
+                        request.thighWidth,
+                        request.bottomTotalLength
+                ))
+                .toList();
     }
 
     private ProductForCreate createProductInfo() {
