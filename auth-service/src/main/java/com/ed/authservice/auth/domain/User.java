a5606b5dfac104b9b89e1a5b39586f59f39609fc
POKUDING <yoseb95@gmail.com>
2025-01-04 19:56:14 +0900

    Refactor: User Entity Change to use @Getter #3

--- auth-service/src/main/java/com/ed/authservice/auth/domain/User.java
+++ auth-service/src/main/java/com/ed/authservice/auth/domain/User.java
@@ -1,7 +1,9 @@
 package com.ed.authservice.auth.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class User {
 
   private final Long id;
@@ -17,21 +19,5 @@ public class User {
     this.username = username;
     this.password = password;
     this.userRole = userRole;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public UserRole getUserRole() {
-    return userRole;
   }
 }
