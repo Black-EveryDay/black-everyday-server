dde8b7d3c239fa41e87c094690a5b01b8bcbe5cb
POKUDING <yoseb95@gmail.com>
2025-01-06 02:05:11 +0900

    Fix: modify table name users -> ed_users #3

--- auth-service/src/main/java/com/ed/authservice/auth/adapter/out/persistence/UserJpaEntity.java
+++ auth-service/src/main/java/com/ed/authservice/auth/adapter/out/persistence/UserJpaEntity.java
@@ -16,7 +16,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "ed_users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserJpaEntity {
