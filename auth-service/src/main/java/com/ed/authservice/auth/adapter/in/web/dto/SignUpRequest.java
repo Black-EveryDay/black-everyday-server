08e1a7f3122def5a38fbcd3f8a499037bf33f5ed
POKUDING <yoseb95@gmail.com>
2025-01-05 17:59:49 +0900

    Fix: remove length restriction from regexp #3 - 중복된 길이제한으로 응답값이 랜덤으로 표현되는 부분을 수정

--- auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/dto/SignUpRequest.java
+++ auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/dto/SignUpRequest.java
@@ -22,7 +22,7 @@ public class SignUpRequest {
   @NotNull(message = "Password cannot be null")
   @Size(min = 8, max = 50, message = "Password must be between 8 and 20 characters")
   @Pattern(
-      regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+      regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
       message = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
   )
   private String password;
