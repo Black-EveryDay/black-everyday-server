c817771a2a78d499ca596944ba1294436615dc8a
POKUDING <yoseb95@gmail.com>
2025-01-06 12:29:54 +0900

    Feat: Add signIn Method to AuthController #8

--- auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/AuthController.java
+++ auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/AuthController.java
@@ -2,9 +2,12 @@ package com.ed.authservice.auth.adapter.in.web;
 
 import static com.ed.authservice.libs.common.ApiResponseUtils.created;
 
+import com.ed.authservice.auth.adapter.in.web.dto.SignInRequest;
 import com.ed.authservice.auth.adapter.in.web.dto.SignUpRequest;
+import com.ed.authservice.auth.application.port.in.AuthSignInCommand;
 import com.ed.authservice.auth.application.port.in.AuthSignUpCommand;
 import com.ed.authservice.auth.application.port.in.AuthUseCase;
+import com.ed.authservice.auth.application.port.out.AuthSignInResponse;
 import com.ed.authservice.auth.application.port.out.AuthSignUpResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
