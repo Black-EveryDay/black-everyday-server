8461deb9550a65a46779460dde60bb1ae1f071bc
POKUDING <yoseb95@gmail.com>
2025-01-06 17:04:59 +0900

    Test: add sign-in tests in AuthControllerTest #8 - 로그인 성공 케이스 - user not found 케이스 - password not match 케이스 - JpaAuditing 에 대한 에러를 발생하지 않도록 JpaMetamodelMappingContext Mock 객체 추가

--- auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/dto/SignInRequest.java
+++ auth-service/src/main/java/com/ed/authservice/auth/adapter/in/web/dto/SignInRequest.java
@@ -1,10 +1,17 @@
 package com.ed.authservice.auth.adapter.in.web.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class SignInRequest {
 
-  private String username;
-  private String password;
+  private final String username;
+  private final String password;
+
+  @Builder
+  private SignInRequest(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
 }
