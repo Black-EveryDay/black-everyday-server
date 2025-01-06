dd1a391956f44c456fca366374480e06039641ef
POKUDING <yoseb95@gmail.com>
2025-01-06 12:38:04 +0900

    Feat: Implement findBBbyUsername method in UserRepository #8

--- auth-service/src/main/java/com/ed/authservice/auth/adapter/out/persistence/UserJpaRepository.java
+++ auth-service/src/main/java/com/ed/authservice/auth/adapter/out/persistence/UserJpaRepository.java
@@ -1,8 +1,11 @@
 package com.ed.authservice.auth.adapter.out.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
 
   boolean existsByUsername(String username);
+
+  Optional<UserJpaEntity> findByUsername(String username);
 }
