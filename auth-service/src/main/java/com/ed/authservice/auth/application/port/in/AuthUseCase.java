f97da1d1f02f56c216590ba4709ba32b3f0d6510
POKUDING <yoseb95@gmail.com>
2025-01-06 12:30:28 +0900

    Feat: Add signIn Method to AuthUseCase #8

--- auth-service/src/main/java/com/ed/authservice/auth/application/port/in/AuthUseCase.java
+++ auth-service/src/main/java/com/ed/authservice/auth/application/port/in/AuthUseCase.java
@@ -1,8 +1,11 @@
 package com.ed.authservice.auth.application.port.in;
 
+import com.ed.authservice.auth.application.port.out.AuthSignInResponse;
 import com.ed.authservice.auth.application.port.out.AuthSignUpResponse;
 
 public interface AuthUseCase {
 
   AuthSignUpResponse signUp(AuthSignUpCommand authSignUpCommand);
+
+  AuthSignInResponse signIn(AuthSignInCommand build);
 }
