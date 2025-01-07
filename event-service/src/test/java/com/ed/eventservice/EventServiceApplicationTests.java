d65ad8be2fd1ee05d63b40b1abf99b89d1f01d2f
POKUDING <yoseb95@gmail.com>
2025-01-07 12:30:31 +0900

    Init: Event-Service #13

---
+++ event-service/src/test/java/com/ed/eventservice/EventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.eventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceApplicationTests {
+
+  @Test
+  void contextLoads() { // default implementation ignored
+  }
+
+}
