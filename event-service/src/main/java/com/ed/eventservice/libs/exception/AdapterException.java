538032c0c3de768499bebd7cf4015119151b5d8b
POKUDING <yoseb95@gmail.com>
2025-01-07 12:31:38 +0900

    Init: init common classes #13

---
+++ event-service/src/main/java/com/ed/eventservice/libs/exception/AdapterException.java
@@ -0,0 +1,8 @@
+package com.ed.eventservice.libs.exception;
+
+public class AdapterException extends BlackEveryDayException {
+
+  public AdapterException(ExceptionStatus exceptionStatus) {
+    super(exceptionStatus);
+  }
+}
