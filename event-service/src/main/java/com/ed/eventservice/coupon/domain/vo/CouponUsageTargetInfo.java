21257afb6a107f9bc2574fe24e6790ccddecac43
POKUDING <yoseb95@gmail.com>
2025-01-07 15:22:09 +0900

    Feat: create CouponTemplate domain #13

---
+++ event-service/src/main/java/com/ed/eventservice/coupon/domain/vo/CouponUsageTargetInfo.java
@@ -0,0 +1,21 @@
+package com.ed.eventservice.coupon.domain.vo;
+
+import com.ed.eventservice.coupon.domain.enums.CouponUsageTargetType;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CouponUsageTargetInfo {
+
+  private final CouponUsageTargetType couponUsageTargetType;
+  private final UUID couponUsageTargetId;
+
+  @Builder
+  private CouponUsageTargetInfo(CouponUsageTargetType couponUsageTargetType,
+      UUID couponUsageTargetId) {
+    this.couponUsageTargetType = couponUsageTargetType;
+    this.couponUsageTargetId = couponUsageTargetId;
+  }
+
+}
