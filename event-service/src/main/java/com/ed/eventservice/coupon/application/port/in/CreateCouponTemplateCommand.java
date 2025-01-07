462f9f6e1f2548799851f58e0bd2456e5641b0e7
POKUDING <yoseb95@gmail.com>
2025-01-07 23:49:07 +0900

    Refactor: change command to coupon template domain convert to mapper #13

--- event-service/src/main/java/com/ed/eventservice/coupon/application/port/in/CreateCouponTemplateCommand.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/application/port/in/CreateCouponTemplateCommand.java
@@ -1,20 +1,16 @@
 package com.ed.eventservice.coupon.application.port.in;
 
-import com.ed.eventservice.coupon.domain.CouponTemplate;
 import com.ed.eventservice.coupon.domain.enums.CouponIssuanceType;
 import com.ed.eventservice.coupon.domain.enums.CouponIssuerType;
 import com.ed.eventservice.coupon.domain.enums.CouponUsageTargetType;
 import com.ed.eventservice.coupon.domain.enums.DiscountType;
-import com.ed.eventservice.coupon.domain.vo.CouponDiscountInfo;
-import com.ed.eventservice.coupon.domain.vo.CouponExpirationInfo;
-import com.ed.eventservice.coupon.domain.vo.CouponIssueInfo;
-import com.ed.eventservice.coupon.domain.vo.CouponUsageTargetInfo;
 import java.math.BigDecimal;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class CreateCouponTemplateCommand {
 
@@ -29,45 +25,5 @@ public class CreateCouponTemplateCommand {
   private BigDecimal discountValue;
   private Long expirationDays;
   private LocalDateTime expirationDate;
-
-  public CouponTemplate toDomain() {
-    return CouponTemplate.builder()
-        .couponName(this.couponName)
-        .couponIssueInfo(this.toCouponIssueInfo())
-        .couponUsageTargetInfo(this.toCouponUsageTargetInfo())
-        .couponDiscountInfo(this.toCouponDiscountInfo())
-        .couponExpirationInfo(this.toCouponExpirationInfo())
-        .build();
-  }
-
-  private CouponIssueInfo toCouponIssueInfo() {
-    return CouponIssueInfo.builder()
-        .couponIssuanceType(this.couponIssuanceType)
-        .couponIssuerType(this.couponIssuerType)
-        .couponIssuerId(this.couponIssuerId)
-        .maxIssuance(this.maxIssuance)
-        .isIssuable(false)
-        .build();
-  }
-
-  private CouponUsageTargetInfo toCouponUsageTargetInfo() {
-    return CouponUsageTargetInfo.builder()
-        .couponUsageTargetType(this.couponUsageTargetType)
-        .couponUsageTargetId(this.couponUsageTargetId)
-        .build();
-  }
-
-  private CouponDiscountInfo toCouponDiscountInfo() {
-    return CouponDiscountInfo.builder()
-        .discountType(this.discountType)
-        .discountValue(this.discountValue)
-        .build();
-  }
-
-  private CouponExpirationInfo toCouponExpirationInfo() {
-    return CouponExpirationInfo.builder()
-        .expirationDays(Duration.ofDays(this.expirationDays))
-        .fixedExpirationDate(this.expirationDate)
-        .build();
-  }
+  private Boolean isIssuable;
 }
--- event-service/src/main/java/com/ed/eventservice/coupon/application/service/CouponService.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/application/service/CouponService.java
@@ -4,6 +4,7 @@ import com.ed.eventservice.coupon.application.port.in.CouponUseCase;
 import com.ed.eventservice.coupon.application.port.in.CreateCouponTemplateCommand;
 import com.ed.eventservice.coupon.application.port.out.CouponTemplatePersistencePort;
 import com.ed.eventservice.coupon.domain.CouponTemplate;
+import com.ed.eventservice.coupon.domain.mapper.CouponTemplateMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,12 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class CouponService implements CouponUseCase {
 
   private final CouponTemplatePersistencePort couponTemplatePersistencePort;
+  private final CouponTemplateMapper couponTemplateMapper;
 
   @Override
   @Transactional
   public void createCouponTemplate(CreateCouponTemplateCommand createCouponTemplateCommand) {
-    CouponTemplate couponTemplate = createCouponTemplateCommand.toDomain();
-    
+    CouponTemplate couponTemplate = couponTemplateMapper.commandToDomain(
+        createCouponTemplateCommand);
     couponTemplatePersistencePort.saveCouponTemplate(couponTemplate);
   }
 }
