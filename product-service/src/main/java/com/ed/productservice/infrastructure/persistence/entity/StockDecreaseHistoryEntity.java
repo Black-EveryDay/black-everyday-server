67e65bb26c8bc1c7ff475145bb0f1e369d0ae3ae
sdongpil <pildong94@naver.com>
2025-01-07 23:09:35 +0900

    Feat: 상품 재고 차감 이력 테이블 추가

---
+++ product-service/src/main/java/com/ed/productservice/infrastructure/persistence/entity/StockDecreaseHistoryEntity.java
@@ -0,0 +1,41 @@
+package com.ed.productservice.infrastructure.persistence.entity;
+
+import com.ed.productservice.domain.vo.ProductCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "ed_stock_decrease_history")
+@Entity
+@NoArgsConstructor
+public class StockDecreaseHistoryEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+    private Integer quantity;
+    private String size;
+
+    @Enumerated(EnumType.STRING)
+    private ProductCategory productCategory;
+
+    private String reservationId;
+
+    public StockDecreaseHistoryEntity(Long productId, Integer quantity, String size,
+        ProductCategory productCategory, String reservationId) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.size = size;
+        this.productCategory = productCategory;
+        this.reservationId = reservationId;
+    }
+}
---
+++ product-service/src/main/java/com/ed/productservice/infrastructure/persistence/repository/StockDecreaseHistoryRepository.java
@@ -0,0 +1,18 @@
+package com.ed.productservice.infrastructure.persistence.repository;
+
+import com.ed.productservice.infrastructure.persistence.entity.StockDecreaseHistoryEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface StockDecreaseHistoryRepository extends
+    JpaRepository<StockDecreaseHistoryEntity, Long> {
+
+    List<StockDecreaseHistoryEntity> findAllByReservationId(String reservationId);
+
+    @Query("select s from StockDecreaseHistoryEntity s where s.reservationId = :reservationId and s.productId = :productId and s.size =:size")
+    List<StockDecreaseHistoryEntity> findByProductIdAndSizeAndReservationId(@Param("productId") Long productId, @Param("size") String size,
+        @Param("reservationId") String reservationId);
+
+}
