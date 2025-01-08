725327cea066778304834be03d75460d64d5680c
POKUDING <yoseb95@gmail.com>
2025-01-09 01:24:14 +0900

    Feat: add createCoupon method in CouponService #13 - add getCouponTemplateByPublicId method in couponTemplatePersistencePort - add updateCoupon method in couponTemplatePersistencePort

--- event-service/src/main/java/com/ed/eventservice/coupon/application/port/out/CouponTemplatePersistencePort.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/application/port/out/CouponTemplatePersistencePort.java
@@ -1,8 +1,13 @@
 package com.ed.eventservice.coupon.application.port.out;
 
 import com.ed.eventservice.coupon.domain.CouponTemplate;
+import java.util.UUID;
 
 public interface CouponTemplatePersistencePort {
 
   CouponTemplate saveCouponTemplate(CouponTemplate couponTemplate);
+
+  CouponTemplate getCouponTemplateByPublicId(UUID couponTemplateId);
+
+  void updateCoupon(CouponTemplate couponTemplate);
 }
