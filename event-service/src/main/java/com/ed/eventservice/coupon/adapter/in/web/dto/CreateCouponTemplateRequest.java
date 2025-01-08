9d5565c15e3f294f31c3c36601856cbfffbc4e5b
POKUDING <yoseb95@gmail.com>
2025-01-08 15:20:03 +0900

    Feat: add validation check annotation in CreateCouponTemplateRequest #13

--- event-service/src/main/java/com/ed/eventservice/coupon/adapter/in/web/dto/CreateCouponTemplateRequest.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/adapter/in/web/dto/CreateCouponTemplateRequest.java
@@ -5,26 +5,41 @@ import com.ed.eventservice.coupon.domain.enums.CouponIssuanceType;
 import com.ed.eventservice.coupon.domain.enums.CouponIssuerType;
 import com.ed.eventservice.coupon.domain.enums.CouponUsageTargetType;
 import com.ed.eventservice.coupon.domain.enums.DiscountType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
+@Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateCouponTemplateRequest {
 
+  @NotNull
   private String couponName;
+  @NotNull
   private CouponIssuanceType couponIssuanceType;
+  @NotNull
   private CouponIssuerType couponIssuerType;
   private UUID couponIssuerId;
   private Integer maxIssuance;
+  @NotNull
   private CouponUsageTargetType couponUsageTargetType;
   private UUID couponUsageTargetId;
+  @NotNull
   private DiscountType discountType;
+  @NotNull
   private BigDecimal discountValue;
+  @Min(1)
   private Long expirationDays;
   private LocalDateTime expirationDate;
 
