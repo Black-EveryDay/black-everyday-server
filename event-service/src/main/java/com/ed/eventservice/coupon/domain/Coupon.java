864dbcc875bfe00ffd4818227efaaedc9c712da6
POKUDING <yoseb95@gmail.com>
2025-01-09 15:56:34 +0900

    Refactor: add CouponTemplate in Coupon #25 - delete couponTemplateId from Coupon

--- event-service/src/main/java/com/ed/eventservice/coupon/domain/Coupon.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/domain/Coupon.java
@@ -1,7 +1,10 @@
 package com.ed.eventservice.coupon.domain;
 
 import com.ed.eventservice.coupon.domain.enums.CouponState;
+import com.ed.eventservice.coupon.domain.enums.CouponStatusChangeReason;
 import com.ed.eventservice.coupon.domain.vo.CouponStatusChangeLog;
+import com.ed.eventservice.libs.exception.DomainException;
+import com.ed.eventservice.libs.exception.ExceptionStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -14,7 +17,7 @@ public class Coupon {
 
   private Long id;
   private UUID publicId;
-  private UUID couponTemplateId;
+  private CouponTemplate couponTemplate;
   private UUID userId;
   private CouponState state;
   private LocalDateTime expirationDate;
