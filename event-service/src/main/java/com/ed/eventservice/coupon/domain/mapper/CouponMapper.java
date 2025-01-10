a681c33b37e6c0ed700199d91ce1e090fa4aa372
POKUDING <yoseb95@gmail.com>
2025-01-10 14:50:35 +0900

    Feat: create CouponStatusChangeLogMapper #25

--- event-service/src/main/java/com/ed/eventservice/coupon/domain/mapper/CouponMapper.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/domain/mapper/CouponMapper.java
@@ -1,8 +1,10 @@
 package com.ed.eventservice.coupon.domain.mapper;
 
 import com.ed.eventservice.coupon.adapter.out.persistence.entity.CouponJpaEntity;
+import com.ed.eventservice.coupon.adapter.out.persistence.entity.CouponStatusChangeLogJpaEntity;
 import com.ed.eventservice.coupon.adapter.out.persistence.entity.CouponTemplateJpaEntity;
 import com.ed.eventservice.coupon.domain.Coupon;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -16,5 +18,6 @@ public interface CouponMapper {
   @Mapping(target = "id", source = "couponJpaEntity.id")
   @Mapping(target = "publicId", source = "couponJpaEntity.publicId")
   Coupon jpaEntityToDomain(CouponJpaEntity couponJpaEntity,
-      CouponTemplateJpaEntity couponTemplateJpaEntity);
+      CouponTemplateJpaEntity couponTemplateJpaEntity,
+      List<CouponStatusChangeLogJpaEntity> couponStatusChangelogs);
 }
