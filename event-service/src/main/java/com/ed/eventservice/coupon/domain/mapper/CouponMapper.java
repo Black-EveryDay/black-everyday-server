418a4d4ea5537808a2ae84d613e2de469f405021
POKUDING <yoseb95@gmail.com>
2025-01-10 14:09:17 +0900

    Feat: add Mapping hint in CouponMapper #25

--- event-service/src/main/java/com/ed/eventservice/coupon/domain/mapper/CouponMapper.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/domain/mapper/CouponMapper.java
@@ -1,13 +1,20 @@
 package com.ed.eventservice.coupon.domain.mapper;
 
 import com.ed.eventservice.coupon.adapter.out.persistence.entity.CouponJpaEntity;
+import com.ed.eventservice.coupon.adapter.out.persistence.entity.CouponTemplateJpaEntity;
 import com.ed.eventservice.coupon.domain.Coupon;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CouponMapper {
 
+  @Mapping(target = "couponTemplateId", source = "coupon.couponTemplate.publicId")
   CouponJpaEntity domainToJpaEntity(Coupon coupon);
 
-  Coupon jpaEntityToDomain(CouponJpaEntity couponJpaEntity);
+  @Mapping(target = "couponTemplate", source = "couponTemplateJpaEntity")
+  @Mapping(target = "id", source = "couponJpaEntity.id")
+  @Mapping(target = "publicId", source = "couponJpaEntity.publicId")
+  Coupon jpaEntityToDomain(CouponJpaEntity couponJpaEntity,
+      CouponTemplateJpaEntity couponTemplateJpaEntity);
 }
