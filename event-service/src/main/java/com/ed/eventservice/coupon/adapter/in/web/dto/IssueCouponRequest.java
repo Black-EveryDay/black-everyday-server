b4327e8b35c77d23fbcc266238eaa3247dc7899c
POKUDING <yoseb95@gmail.com>
2025-01-14 12:22:20 +0900

    Test: create CouponControllerTest #44

--- event-service/src/main/java/com/ed/eventservice/coupon/adapter/in/web/dto/IssueCouponRequest.java
+++ event-service/src/main/java/com/ed/eventservice/coupon/adapter/in/web/dto/IssueCouponRequest.java
@@ -1,9 +1,11 @@
 package com.ed.eventservice.coupon.adapter.in.web.dto;
 
 import java.util.UUID;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class IssueCouponRequest {
 
   private UUID userId;
