9ff677ff695e4f1481ce3d41564444c1f682213c
lowgiant <lowgiant@gmail.com>
2025-01-21 13:06:04 +0900

    feat: Add JPA auditing and base time entity configuration
    - BaseTimeByJpaEntity.java: 데이터 감사로그
    - BaseTimeJpaEntity.java: 시간 정보만 담김 엔티티
    - JpaAuditingConfig.java: JPA Auditing 설정 파일

---
+++ order-service/src/main/java/com/ed/orderservice/infrastructure/entity/common/BaseTimeByJpaEntity.java
@@ -0,0 +1,32 @@
+package com.ed.orderservice.infrastructure.entity.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTimeByJpaEntity extends BaseTimeJpaEntity {
+
+  @Column(name = "CREATED_BY", length = 36)
+  private String createdBy;
+
+  @Column(name = "UPDATED_BY", length = 36)
+  private String updatedBy;
+
+  @Column(name = "DELETED_BY", length = 36)
+  private String deletedBy;
+
+  public void createBy(String userUuid) {
+    this.createdBy = userUuid;
+  }
+
+  public void updateBy(String userUuid) {
+    this.updatedBy = userUuid;
+  }
+
+  public void softDeleteBy(String userUuid) {
+    super.softDelete();
+    this.deletedBy = userUuid;
+  }
+}
---
+++ order-service/src/main/java/com/ed/orderservice/infrastructure/entity/common/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.ed.orderservice.infrastructure.entity.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
