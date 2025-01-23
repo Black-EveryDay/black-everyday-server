3093d5a986e73fb6ae6fd6679f2d15f5e4d0fa29
sdongpil <pildong94@naver.com>
2025-01-23 18:05:28 +0900

    Feat: 인기 상품 Redis 캐싱

--- product-service/src/main/java/com/ed/productservice/infrastructure/persistence/scheduler/SchedulerUtil.java
+++ product-service/src/main/java/com/ed/productservice/infrastructure/persistence/scheduler/SchedulerUtil.java
@@ -1,9 +1,13 @@
 package com.ed.productservice.infrastructure.persistence.scheduler;
 
 import com.ed.productservice.infrastructure.persistence.entity.StockDecreaseHistoryEntity;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,5 +18,23 @@ public class SchedulerUtil {
     return uncommittedStocks.stream().map(
             StockDecreaseHistoryEntity::getTransactionId)
         .collect(Collectors.toSet());
+  }
+
+  public TimeRange getTimeRangeForCache() {
+    return new TimeRange();
+  }
+
+  @Getter
+  public static class TimeRange {
+
+    private final String startTime;
+    private final String endTime;
+
+    private TimeRange() {
+      this.startTime = ZonedDateTime.now(ZoneOffset.UTC).minusHours(1)
+          .format(DateTimeFormatter.ISO_DATE_TIME);
+      this.endTime = ZonedDateTime.now(ZoneOffset.UTC)
+          .format(DateTimeFormatter.ISO_DATE_TIME);
+    }
   }
 }
