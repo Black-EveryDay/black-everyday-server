9765e64b3fb24b3f803ed262075b03940f01ca57
lowgiant <lowgiant@gmail.com>
2025-01-27 02:35:57 +0900

    feat: add metrics to coupon

--- coupon-service/src/main/java/com/ed/couponservice/libs/common/ResponseWrapper.java
+++ coupon-service/src/main/java/com/ed/couponservice/libs/common/ResponseWrapper.java
@@ -1,5 +1,6 @@
 package com.ed.couponservice.libs.common;
 
+import org.springframework.beans.factory.annotation.Value;
 import com.ed.couponservice.libs.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
@@ -7,12 +8,24 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ResponseWrapper implements ResponseBodyAdvice<Object> {
+
+  private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+  @Value("${management.endpoints.web.base-path}")
+  private String metricsBasePath;
+
+  private boolean isExcludedPath(ServerHttpRequest request) {
+    String path = request.getURI().getPath();
+    String patternToMatch = metricsBasePath + "/prometheus/**";
+    return pathMatcher.match(patternToMatch, path);
+  }
 
   @Override
   public boolean supports(
@@ -27,6 +40,10 @@ public class ResponseWrapper implements ResponseBodyAdvice<Object> {
       Class<? extends HttpMessageConverter<?>> selectedConverterType,
       ServerHttpRequest request,
       ServerHttpResponse response) {
+
+    if (isExcludedPath(request)) {
+      return body;
+    }
 
     if (body instanceof ErrorResponse) {
       return new ApiResponse<>(false, body);
