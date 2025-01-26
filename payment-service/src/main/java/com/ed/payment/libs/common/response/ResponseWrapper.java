8a70ac6f3b1e7437a3344341aa8e7684d673130e
lowgiant <lowgiant@gmail.com>
2025-01-27 02:32:03 +0900

    feat: add metrics to payment

--- payment-service/src/main/java/com/ed/payment/libs/common/response/ResponseWrapper.java
+++ payment-service/src/main/java/com/ed/payment/libs/common/response/ResponseWrapper.java
@@ -1,17 +1,31 @@
 package com.ed.payment.libs.common.response;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ResponseWrapper implements ResponseBodyAdvice<Object> {
+
+  private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+  @Value("${management.endpoints.web.base-path}")
+  private String metricsBasePath;
+
+  private boolean isExcludedPath(ServerHttpRequest request) {
+    String path = request.getURI().getPath();
+    String patternToMatch = metricsBasePath + "/prometheus/**";
+    return pathMatcher.match(patternToMatch, path);
+  }
+
   @Override
   public boolean supports(
       MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -19,13 +33,16 @@ public class ResponseWrapper implements ResponseBodyAdvice<Object> {
   }
 
   @Override
-  public Object beforeBodyWrite(
-      Object body,
+  public Object beforeBodyWrite(Object body,
       MethodParameter returnType,
       MediaType selectedContentType,
       Class<? extends HttpMessageConverter<?>> selectedConverterType,
       ServerHttpRequest request,
       ServerHttpResponse response) {
+
+    if (isExcludedPath(request)) {
+      return body;
+    }
 
     if (body instanceof ErrorResponse) {
       return new ApiResponse<>(false, body);
